580af4fe8e03e40660624581e358027bf9a90509
elad.shem-tov <eladgoodname@gmail.com>
2023-02-02 15:59:05 +0200

    functions added

--- c/expressions/expressions.h
+++ c/expressions/expressions.h
@@ -5,23 +5,43 @@ printf("%c%c%c%c%c%c%c%c%c%c%c%c%c\n",0X22,0X48,0X65,0X6C,0X6C,0X6F,0X20,0X57,0X
 
 float GivePower(int n)
 {
-	float x = 10;
+	float base = 10;
 	float result = 1;
 
-	if ( n < 0 )
+	if ( n < result )
 	{
-		x = 1/10;
+		base = 1/base;
 		n *= -1;
 	}
 	
 	while( n > 0 )
 	{
-		result = result * x;
+		result *= base;
 		n--;
 	}
 		
 	return result;
 }
  
- 
 
+int FlipDigits(int n)
+{
+
+	int flipped;
+
+	while( n > 0 )
+	{
+		flipped = flipped * 10 + n%10;
+		n /= 10;
+	}
+
+	return flipped;
+}
+
+void SwapValues(int *a, int *b)
+{
+	int temp = *a;
+	assert( a != NULL && b != NULL );
+	*a = *b;
+	*b = temp;
+}
--- c/expressions/main.c
+++ c/expressions/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <assert.h>
 #include "expressions.h"
 
 int main()
 {
-	printf("%f",GivePower(-1));
+	printf("all of function tested!");
 	return 0;
 }
 
