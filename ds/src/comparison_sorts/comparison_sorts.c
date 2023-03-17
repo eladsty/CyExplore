f99b9c227dae78a949a44c8fca7ca4faa4a577f1
elad.shem-tov <eladgoodname@gmail.com>
2023-03-17 17:49:55 +0200

    update sorting

--- ds/src/comparison_sorts/comparison_sorts.c
+++ ds/src/comparison_sorts/comparison_sorts.c
@@ -4,9 +4,9 @@
 
 static void swap(int *n1, int *n2)
 {
-	int temp = *n1;
-	*n1 = *n2;
-	*n2 = temp;
+	*n1 ^= *n1 ^ *n2;
+	*n2 ^= *n1 ^ *n2;
+	*n1 ^= *n1 ^ *n2;
 }
 /*
 	status: 
--- ds/test/comparison_sorts/comparison_sorts_test.c
+++ ds/test/comparison_sorts/comparison_sorts_test.c
@@ -1,9 +1,11 @@
 #include <stddef.h>
+#include <time.h>/*show the time*/
 #include <stdio.h> /*printf*/
 
 #include "comparison_sorts.h"
 
 int int_arr[5000] = {0};
+
 void MakeRandomArr_5K(int *arr)
 {	
 	int random_n = 0;
@@ -31,35 +33,47 @@ void PrintArr_5k(int *arr)
 
 int main()
 {
-	/*presort*/
-	printf("***random array below***\n\n\n\n");
+  	clock_t tic_bub = 0;
+  	clock_t toc_bub = 0;
+	clock_t tic_sel = 0;
+  	clock_t toc_sel = 0;	 
+  	clock_t tic_ins = 0;
+  	clock_t toc_ins = 0;
+
+	/*presort - created by MakeRandom*/
  	MakeRandomArr_5K(int_arr);
  	PrintArr_5k(int_arr);
 	/*postsort*/
 	printf("***Bubble sorted array below***\n\n\n\n");
+	tic_bub = clock();
 	BubbleSort(int_arr, 5000);
+	toc_bub = clock();
 	PrintArr_5k(int_arr);
 	
 	
-	/*presort*/
+	/*presort - created by MakeRandom*/
 	printf("***random array below***\n\n\n\n");
  	MakeRandomArr_5K(int_arr);
 	PrintArr_5k(int_arr);
 	/*postsort*/
 	printf("***Selection sorted array below***\n\n\n\n");
+	tic_sel = clock();
 	SelectionSort(int_arr, 5000);
+	toc_sel = clock();
 	PrintArr_5k(int_arr);
 	
-	/*presort*/
+	/*presort - created by MakeRandom*/
 	printf("***random array below***\n\n\n\n");
  	MakeRandomArr_5K(int_arr);
 	PrintArr_5k(int_arr);
 	/*postsort*/
 	printf("***Insertion sorted array below***\n\n\n\n");
+	tic_ins = clock();
 	InsertionSort(int_arr, 5000);
+	toc_ins = clock();
 	PrintArr_5k(int_arr);
-	
-	
-	
+	printf("Time elapsed for BubbleSort: %f seconds\n",(double)(toc_bub - tic_bub) / CLOCKS_PER_SEC);
+	printf("Time elapsed for SelectionSort: %f seconds\n", (double)(toc_sel - tic_sel) / CLOCKS_PER_SEC);
+	printf("Time elapsed for InsertionSort: %f seconds\n", (double)(toc_ins - tic_ins) / CLOCKS_PER_SEC);
 	return 0;
 }
