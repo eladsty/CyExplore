33a7d0472b24c522b75457f5d3e015d2aeed69cf
elad.shem-tov <eladgoodname@gmail.com>
2023-03-18 17:33:13 +0200

    IsRotation

--- quizzes/5_IsRotation/main.c
+++ quizzes/5_IsRotation/main.c
@@ -1,39 +1,40 @@
 #include <string.h> /*strlen*/
 #include <stdio.h>
-/*
-int StrCmpInRotation(char *str1, char *str2, int len)
+ 
+int StrCmpInRotation(char *str1, char *str2, int n, int len)
 {
 	int i = 0;
-	while('\0' != str1[i] && str2[i % len] == str1[i])
+	int j = 0;
+	j = n;
+	while('\0' != str1[i] && str2[j % len] == str1[i])
 	{
 		 ++i;
+		 ++j;
 	}
 	
-	if('\0' == str1[i])
+	if('\0' == str1[i] && '\0' && strlen(str2) == len)
 	{
 		return 1;
 	}
 	
 	return 0;
 }
-*/
+ 
 int IsRotation(char *str1, char *str2)
 {
 	int len = strlen(str1); 
-	int len2 = strlen(str2);
-	int i = 0, int j = 0; 
-	if(len != len2)
-	{
-		return 0;
-	}
+	int n = 0; 
 
-	while('\0' != str2[j])
+	while('\0' != str2[n])
 	{
-		if( str1[i] == str2[j])
+		if( str1[0] == str2[n])
 		{
-			 
+			 if(1 == StrCmpInRotation(str1,str2,n,len))
+			 {
+			 	return 1;
+			 }
 		}
-		++j;
+		++n;
 	}	
 	
 	 
@@ -43,11 +44,9 @@ int IsRotation(char *str1, char *str2)
 int main()
 {
 	char *arr_match[] = {"abcde","deabc","cdeab","cdeab","cdeab" };
-	char *arr_no_match[] = {"", "abcde","deabc","cdeab","deacbd" };
+	char *arr_no_match[] = {"", "abcde","deaeebc","cdeeab","deacbd" };
 	int i = 0;
-	
-	printf("should be 1: %d",IsRotation("abcde","deabc"));  
-	/*
+ 
 	for(i = 0;i < 4 ; ++i)
 	{
 		printf("should be 1: %d\n",IsRotation(arr_match[i],arr_match[i+1]));
@@ -56,6 +55,6 @@ int main()
 	{
 		printf("should be 0: %d\n",IsRotation(arr_no_match[i],arr_no_match[i+1]));
 	}
-	*/
+ 		printf("should be 0: %d\n",IsRotation("abcdefg","bcdefgabcde"));
 	return 0;
 }
