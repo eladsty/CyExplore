8eb6e1d8997881e1b8367737a40db38241e42094
elad.shem-tov <eladgoodname@gmail.com>
2023-03-22 09:38:59 +0200

    asd

--- system_programming/src/vsa/vsa.c
+++ system_programming/src/vsa/vsa.c
@@ -29,7 +29,8 @@ struct vsa
 
 /*
  this function received vsa_t * and retunrs info_block (type long)
-block_info contains msb 
+if BlockInfor returns negative, that means the block is free
+
 */
 long BlockInfo(vsa_t *block)
 {
@@ -40,35 +41,77 @@ vsa_t *VSAInit(void *memory_p, size_t memory_size)
 {
 	vsa_t *new_vsa = (vsa_t *)((char *)ALIGNTOWORD((size_t)memory_p));
 	size_t aligned_mem = MEMSIZEALIGN(memory_size);
-	
-	if(NULL == memory_p && aligned_mem <= 24)
-	{	
-		printf("invalid memory address or insufficient memory size.");
-		return; /*should exit*/
-	}
-	aligned_mem -= (sizeof(vsa_t) + sizeof(vsa_t));
+	aligned_mem -= (sizeof(vsa_t));
 	/*the block is now free to use*/
 	new_vsa->block_info = -1 * aligned_mem;
 	
 	/*the offset is now pointing to ENDOFMEM*/
-	*((long *)((size_t)new_vsa + new_vsa->block_info * -1)) = -1 * ENDOFMEM;
+	*((long *)((char *)new_vsa + new_vsa->block_info * -1)) = ENDOFMEM;
 	return new_vsa;
+}
+/*
+	send it empty block and get total size of defraged spaced
+	if BlockInfor returns negative, that means the block is free
+*/
+static size_t Defrag(vsa_t *block)
+{
+	size_t total_size = 0;
+	vsa_t *iter = NULL;
+	iter = (vsa_t *)block;
+	while(iter->block_info < 0 && iter->block_info != ENDOFMEM)
+	{
+		total_size += BlockInfo(iter) * -1;
+		iter = (vsa_t *)(char *)block + total_size;
+	}
+	
+	block->block_info = total_size * -1;
+	return total_size;
 }
 
 void *VSAAlloc(vsa_t *vsa_pool, size_t block_size)
 {
-	size_t aligned_mem = MEMSIZEALIGN(memory_size);
-	
-	if(NULL == vsa_pool && aligned_mem <= sizeof(vsa_t))
-	{	
-		printf("invalid memory address or insufficient memory size.");
-		return; /*should exit*/
-	}
+	vsa_t *iter = NULL;
+	long available_size = 0; 			/*added wordsize for block_info section*/
+	long aligned_block_size = ALIGNTOWORD(block_size) + WORDSIZE;
+	iter = vsa_pool;
 
-	while()
+	while(BlockInfo(iter) != ENDOFMEM)
 	{
-
+		available_size = Defrag(iter);
+		if(BlockInfo(iter) < 0 && available_size >= aligned_block_size)
+		{	/* ad extra WORDSIZE is added to the return to avoid overwriting blockinfo section*/
+			iter->block_info = aligned_block_size;
+			if(available_size > aligned_block_size)
+			{
+				*(long *)((char *)iter + aligned_block_size) = (available_size - aligned_block_size) * -1;
+			}
+ 			return (void *)((char *)iter + WORDSIZE);
+		}
+		iter = (vsa_t *)(char *)iter + iter->block_info;
 	}
-
 	return NULL;
+}
+
+ 
+
+void VSAFree(void *block)
+{
+	*(long *)((char *)block - WORDSIZE) *= -1;
+}
+
+size_t VSALargestBlockAvailable(const vsa_t *vsa_pool)
+{
+ 	vsa_t *iter = (vsa_t *)vsa_pool;
+	long temp;
+	long largest_size = 0;
+	while(0 > BlockInfo(iter) && BlockInfo(iter) != ENDOFMEM)
+	{
+		temp = Defrag(iter);
+		if(temp > largest_size)
+		{
+			largest_size = temp;
+		}
+		iter = (vsa_t *)(char *)iter + iter->block_info;
+ 	}
+	return largest_size;
 }
--- system_programming/test/vsa/vsa_test.c
+++ system_programming/test/vsa/vsa_test.c
@@ -1,23 +1,45 @@
 #include <stdio.h> /* for printf*/
 #include <stdlib.h> /*malloc*/
-
+#include <string.h> /* for memcpy*/
 #include "vsa.h"
 
 int main()
 {
 	void *vsa_start_pool = malloc(1097);
 	vsa_t *new_vsa = NULL;
+	vsa_t *vsa_cpy_for_info = NULL;
 	int int_arr[] = {123123,234234,345345,456456,567567};
+	char *string_of_90 = "klmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy";
+	char *string_of_100 = "abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy";
+	char string_buffer[100]; 
+	char *str_for_alloc;
 	long block_info_print;
 	if(NULL == vsa_start_pool)
 	{
 		return -1;
 	}
+	
  	new_vsa = VSAInit(vsa_start_pool, 5097);
  	printf("the size of the pool after alignment is: %d\n", BlockInfo(new_vsa)); 
  	printf("endofmem is: %d\n", *((long *)((size_t)new_vsa + BlockInfo(new_vsa) * -1))); 
+	
+	str_for_alloc = (char *)VSAAlloc(new_vsa, 100);
+	/*
+	vsa_cpy_for_info = (vsa_t *)((char *)str_for_alloc - 8);
+	printf("should return 100: %d\n", BlockInfo(vsa_cpy_for_info); 
+	*/
 
+	strcpy(str_for_alloc, string_of_90);
+
+	printf("str_for_alloc: %s\n", str_for_alloc);
+	VSAFree(str_for_alloc);
+ 	printf("str_for_alloc: %s\n", str_for_alloc);
+ 	printf("the size of the pool after alignment is: %d\n", BlockInfo(new_vsa)); 
+ 	printf("largest block is : %d\n", VSALargestBlockAvailable(new_vsa)); 
+ 
 	free(vsa_start_pool);
 	vsa_start_pool = NULL;
 	return 0;
 }
+
+ 
