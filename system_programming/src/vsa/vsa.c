d557f447b6d2bd1c3b6b169556d06c43a09cc2e4
elad.shem-tov <eladgoodname@gmail.com>
2023-03-23 00:11:16 +0200

    vsa.c

--- system_programming/src/vsa/vsa.c
+++ system_programming/src/vsa/vsa.c
@@ -5,7 +5,7 @@
 #define ALIGNTOWORD(num)((0 == num % WORDSIZE) ? num : ((num >> 3) << 3) + WORDSIZE)
 #define MEMSIZEALIGN(mem_size)(0 == mem_size % WORDSIZE ? mem_size : mem_size - (mem_size % WORDSIZE))
 #define ENDOFMEM -97654321
-#define ABS(n)(n < 0 ? n * -1: n)
+#define ABS(x) (((x) < 0) ? -(x) :(x))
  /*
  ONOFFMSB  turn on/off the leftmost bit (which represent if a block is occupied).
  Due to leftmost bit being used for this purpose, the largest number of bytes we can use is 2^31
@@ -42,52 +42,57 @@ vsa_t *VSAInit(void *memory_p, size_t memory_size)
 {
 	vsa_t *new_vsa = (vsa_t *)((char *)ALIGNTOWORD((size_t)memory_p));
 	size_t aligned_mem = MEMSIZEALIGN(memory_size);
+	vsa_t *end_of_memory = NULL;
 	aligned_mem -= (sizeof(vsa_t));
 	/*the block is now free to use*/
 	new_vsa->block_info = -1 * aligned_mem;
-	
 	/*the offset is now pointing to ENDOFMEM*/
-	*((long *)((char *)new_vsa + new_vsa->block_info * -1)) = ENDOFMEM;
+	end_of_memory = ((vsa_t *)((char *)new_vsa + new_vsa->block_info));
+	end_of_memory->block_info = ENDOFMEM;
 	return new_vsa;
 }
 /*
-	send it empty block and get total size of defraged spaced
-	if BlockInfor returns negative, that means	 the block is free*/
-static size_t Defrag(vsa_t *block)
+returns pointer to vsa_t, after concatenating multiple adjacent empty blocks (if available)
+*/
+static vsa_t *Defrag(vsa_t *block)
 {
-	size_t total_size = 0;
-	vsa_t *iter = NULL;
-	iter = (vsa_t *)block;
+	vsa_t *iter = (vsa_t *)block;
+	  
 	while(iter->block_info < 0 && iter->block_info != ENDOFMEM)
 	{
-		total_size += (iter->block_info * -1);
- 		iter = (vsa_t *)((char *)block + total_size);
+ 		iter = (vsa_t *)((char *)block + ABS(iter->block_info));
+		if(iter->block_info < 0 && iter->block_info != ENDOFMEM)
+		{
+			block->block_info -= iter->block_info;
+		}
 	}
-	return total_size;
+	return block;
 }
 
 void *VSAAlloc(vsa_t *vsa_pool, size_t block_size)
 {
 	vsa_t *iter = NULL;
-	long available_size = 0; 			/*added wordsize for block_info section*/
+	long available_size = vsa_pool->block_info; 
+	 /*added wordsize for block_info section*/
 	long aligned_block_size = ALIGNTOWORD(block_size) + WORDSIZE;
-	iter = vsa_pool;
 
-	while(iter->block_info != ENDOFMEM)
+	 /*iterate, until First Match or end of memory*/
+	while(available_size < aligned_block_size && iter->block_info != ENDOFMEM)
 	{
-		available_size = Defrag(iter);
-		if(BlockInfo(iter) < 0 && available_size >= aligned_block_size)
-		{	/* ad extra WORDSIZE is added to the return to avoid overwriting blockinfo section*/
-			iter->block_info = aligned_block_size;
-			if(available_size > aligned_block_size)
-			{
-				*(long *)((char *)iter + aligned_block_size) = (available_size - aligned_block_size) * -1;
-			}
- 			return (void *)((char *)iter + WORDSIZE);
-		}
-		iter = (vsa_t *)(char *)iter + iter->block_info;
+		vsa_pool = (vsa_t *)((char *)vsa_pool + ABS(iter->block_info));  
+		vsa_pool = Defrag(vsa_pool);  
+		available_size = vsa_pool->block_info;
 	}
-	return NULL;
+
+	if(available_size > (long)block_size)
+	{
+		iter = (vsa_t *)((char *)vsa_pool + block_size);
+		iter->block_info = (available_size - block_size);
+	}
+	
+	vsa_pool->block_info = ((long)block_size * -1);
+
+    return (vsa_pool + 1);
 }
 
  
@@ -100,12 +105,13 @@ void VSAFree(void *block)
 size_t VSALargestBlockAvailable(const vsa_t *vsa_pool)
 {
  	vsa_t *iter = (vsa_t *)vsa_pool;
-	size_t largest_size = 0;
-	while(iter->block_info != ENDOFMEM)
+	long largest_size = 0;
+ 	while(iter->block_info != ENDOFMEM)
 	{
-		if(0 > iter->block_info && Defrag(iter) > largest_size)
+		iter = Defrag((vsa_t *)vsa_pool);
+		if(largest_size < ABS(iter->block_info))
 		{
-			largest_size =  Defrag(iter);
+			largest_size = ABS(iter->block_info);
 		}
 		iter = (vsa_t *)((char *)iter + ABS(iter->block_info));
  	}
