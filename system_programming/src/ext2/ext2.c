fc16259acbd300a3d3fca174882b8d54b9460d87
elad <eladgoodname@gmail.com>
2023-04-17 10:21:15 +0300

    ext2

--- system_programming/src/ext2/ext2.c
+++ system_programming/src/ext2/ext2.c
@@ -1,29 +1,72 @@
 
- #include <unistd.h>/* read */
-
+ #include <unistd.h>/*  seek read */
+#include <assert.h>
+#include <stdlib.h>
+#include <fcntl.h>/*open  */
+#include <stdio.h>
 #include "/home/elad/elad.shem-tov/system_programming/include/ext2.h"
 #include "/home/elad/elad.shem-tov/system_programming/include/ext2_linux.h"
  
- struct session_handler 
+#define BASE_OFFSET
+#define BASE_OFFSET 1024
+#define BLOCK_OFFSET(block) (BASE_OFFSET+(block-1)*block_size)
+
+ struct process 
 {
+    int fd;
     struct ext2_super_block *sb;
 };
 
-/* struct process
+handle_t *EXT2Open(char *device)
 {
+    ssize_t a = 0;
+    inode_t fd = open(device, O_RDWR);
+    handle_t *handle = (handle_t *)malloc( sizeof(handle_t) );
+    assert(NULL != handle);
 
-};  
- */
+    handle->fd = fd;
+    lseek(fd, BASE_OFFSET, SEEK_SET);
+    handle->sb = (struct ext2_super_block *)malloc( sizeof(struct ext2_super_block) );
 
-/* handle_t *EXT2Open(char *device)
-{
-    char buff[4097] = {0};
-    fd = open("/dev/ram0", O_RDWR);
-    lseek(fd, 1024, SEEK_SET);
 
-    read(fd, buff, 4097);
-    printf("%s\n", buff);
-    close(buff);
+    printf("file descriptor is %ld\n",fd);
+
+    if(NULL == handle->sb)
+    {
+        free(handle);
+        handle = NULL;
+        return NULL;
+    }
+
+    a = read(fd, handle->sb, sizeof(struct ext2_super_block));
+    if(-1 == a )
+    {
+        return NULL;
+    }  
+    return handle;
 }
 
-  */
+int EXT2Close(handle_t *process)
+{
+    if (NULL == process)
+	{
+		return -1;
+	}
+    
+    close(process->fd);
+
+    free(process->sb);
+    process->sb = NULL;
+
+    free(process);
+    process = NULL;
+ 
+    return 0;
+}
+
+inode_t EXT2GetFileDescriptor(handle_t *process, char *file_path)
+{
+
+    BASE_OFFSET
+ 
+}
