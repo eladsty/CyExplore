9f9d51f97910ce2f563e4c0763e2a0f1326552ea
elad <eladgoodname@gmail.com>
2023-05-03 23:03:15 +0300

    ex2

--- system_programming/src/threading/ex2/main.c
+++ system_programming/src/threading/ex2/main.c
@@ -1,95 +1,106 @@
 
 #include <pthread.h>/* threads */
 #include <threads.h>/* threads */
- #include <stdio.h>  /* printf */
+#include <stdio.h>  /* printf */
 #include <stdlib.h>
-#include <omp.h>  /* OpenMP */
+#include <omp.h>    /* OpenMP */
 #include <assert.h> /* assert */
 #include "slist.h"
 #include "slist.c" /* if time permits, I should create a shared library */
 
 #define MAX_ROUNDS 10
 
+/* 
+description: function pointer to: function that returns void pointer (data to insert)
+ */
+typedef void *(*produce_action_t(void* data)) ;
 
-slist_t *new_list = NULL;
-pthread_mutex_t mutex_lock;
 
+struct thread_struct
+{
+    slist_t *new_list;
+    pthread_mutex_t mutex_lock;
+    produce_action_t *action_func;
+ };
 
+ 
 
-
-void *Consumer()
+void *Consumer(struct thread_struct *thr_struct)
 {
     size_t j = 0;
     int i = 0;
-    size_t answer = 0;    
+    size_t answer = 0;
     slist_iter_t head_node = NULL;
-    
-     while (j < MAX_ROUNDS) 
+
+    while (j < MAX_ROUNDS)
     {
-        pthread_mutex_lock(&mutex_lock);
-        if(SListSize(new_list) > 0)
+        pthread_mutex_lock(&thr_struct->mutex_lock);
+        if (SListSize(thr_struct->new_list) > 0)
         {
-            head_node =  SListStart(new_list);
-            answer = (size_t)SListGetData( head_node );
-            printf(" %ld \n",answer); 
+            head_node = SListStart(thr_struct->new_list);
+            answer = (size_t)SListGetData(head_node);
+            printf(" %ld \n", answer);
             SListDelete(head_node);
             ++j;
         }
-        pthread_mutex_unlock(&mutex_lock);
-     }    
-
-
-    return NULL;
- }
-
-void *Producer()
-{
-    size_t j = 0;
-    int i = 0;
-    int *rand_n = NULL;
-    slist_iter_t head_node = NULL;
-
-    while(j < MAX_ROUNDS)
-    {
-        pthread_mutex_lock(&mutex_lock);
-
-        head_node = SListStart(new_list);
-
-        SListInsertBefore(head_node , (void*)j);
-        ++j;
-
-        pthread_mutex_unlock(&mutex_lock);
+        pthread_mutex_unlock(&thr_struct->mutex_lock);
     }
 
     return NULL;
 }
 
+ void *ProduceData()
+{        
+    return (rand() % 10);   
+}
+
+void *Producer(struct thread_struct *thr_struct)
+{
+    int i = 0;
+    slist_iter_t head_node = NULL;
+    void *data = thr_struct->action_func(NULL);
+    while (i < MAX_ROUNDS)
+    {
+        pthread_mutex_lock(&thr_struct->mutex_lock);
+
+        head_node = SListStart(thr_struct->new_list);
+        SListInsertBefore(head_node, data );
+        ++i;
+
+        pthread_mutex_unlock(&thr_struct->mutex_lock);
+    }
+
+    return NULL;
+}
 
 void ProdConsMain()
 {
     pthread_t cons_thr[3] = {0};
     pthread_t prod_thr[3] = {0};
     int i = 0;
+    struct thread_struct thr_strct;
+    pthread_mutex_t mutex_lock;
 
-        
-    for(i = 0; i < 3; ++i)
+    thr_strct.mutex_lock = mutex_lock;
+    thr_strct.new_list = SListCreate();
+    thr_strct.action_func = ProduceData;
+    for (i = 0; i < 3; ++i)
     {
-        pthread_create(&prod_thr[i], NULL, Producer, NULL );
-        pthread_create(&cons_thr[i], NULL, Consumer, NULL );
+        pthread_create(&prod_thr[i], NULL, Producer, &thr_strct);
+        pthread_create(&cons_thr[i], NULL, Consumer, &thr_strct);
     }
-    
-    for(i = 0; i < 3; ++i)
+
+    for (i = 0; i < 3; ++i)
     {
         pthread_join(cons_thr[i], NULL);
         pthread_join(prod_thr[i], NULL);
-    }    
- }
-
+    }
+}
 
 int main()
 {
-    new_list = SListCreate();
     ProdConsMain();
-     SListDestroy(new_list);
-  return 0;
+    /*      SListDestroy(new_list);
+     */
+    return 0;
 }
---
+++ system_programming/src/threading/ex2/main_copy.c
@@ -0,0 +1,95 @@
+#include <pthread.h>/* threads */
+#include <threads.h>/* threads */
+ #include <stdio.h>  /* printf */
+#include <stdlib.h>
+#include <omp.h>  /* OpenMP */
+#include <assert.h> /* assert */
+#include "slist.h"
+#include "slist.c" /* if time permits, I should create a shared library */
+
+#define MAX_ROUNDS 10
+
+
+slist_t *new_list = NULL;
+pthread_mutex_t mutex_lock;
+
+
+
+
+void *Consumer()
+{
+    size_t j = 0;
+    int i = 0;
+    size_t answer = 0;    
+    slist_iter_t head_node = NULL;
+    
+     while (j < MAX_ROUNDS) 
+    {
+        pthread_mutex_lock(&mutex_lock);
+        if(SListSize(new_list) > 0)
+        {
+            head_node =  SListStart(new_list);
+            answer = (size_t)SListGetData( head_node );
+            printf(" %ld \n",answer); 
+            SListDelete(head_node);
+            ++j;
+        }
+        pthread_mutex_unlock(&mutex_lock);
+     }    
+
+
+    return NULL;
+ }
+
+void *Producer()
+{
+    size_t j = 0;
+    int i = 0;
+    int *rand_n = NULL;
+    slist_iter_t head_node = NULL;
+
+    while(j < MAX_ROUNDS)
+    {
+        pthread_mutex_lock(&mutex_lock);
+
+        head_node = SListStart(new_list);
+
+        SListInsertBefore(head_node , (void*)j);
+        ++j;
+
+        pthread_mutex_unlock(&mutex_lock);
+    }
+
+    return NULL;
+}
+
+
+void ProdConsMain()
+{
+    pthread_t cons_thr[3] = {0};
+    pthread_t prod_thr[3] = {0};
+    int i = 0;
+
+        
+    for(i = 0; i < 3; ++i)
+    {
+        pthread_create(&prod_thr[i], NULL, Producer, NULL );
+        pthread_create(&cons_thr[i], NULL, Consumer, NULL );
+    }
+    
+    for(i = 0; i < 3; ++i)
+    {
+        pthread_join(cons_thr[i], NULL);
+        pthread_join(prod_thr[i], NULL);
+    }    
+ }
+
+
+int main()
+{
+    new_list = SListCreate();
+    ProdConsMain();
+     SListDestroy(new_list);
+  return 0;
+}
+
