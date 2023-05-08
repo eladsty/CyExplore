6ed8074dba8628a6994aa773fd635b0155d68fc3
elad <eladgoodname@gmail.com>
2023-05-08 16:08:37 +0300

    python

--- python/Data_structures.py
+++ python/Data_structures.py
@@ -1,17 +1,17 @@
-""" Approved by Amir """
-
 # EX1
 
-def string_extractor(list_ts):
-    return [elem for elem in list_ts if type(elem) == str]
+def string_extractor(list):
+    for elem in list:
+        if isinstance(elem, str) == False:
+            list.remove(elem)
  
-list_test = [4,"hello", False, "world"]
+list = [4,"hello", False, "world"]
 
-string_extractor(list_test)
+string_extractor(list)
 
-print(string_extractor(list_test))
+print(list)
 
-""" Approved by Amir """
+
 # EX2
 
 def str_to_dic(str):
@@ -20,38 +20,35 @@ dictionary = str_to_dic("basasababa")
 
 print(dictionary)
 
-
-""" APPROVED BY AMIR """
 # EX3
 
 def same_elem(list1, list2):
-    return list(set(list1) & set(list2))
+    return (set(list1) & set(list2))
 
 list1 = [1,3,4,5]
 list2 = [2,3,5,7]
 listy  = same_elem(list1, list2) 
 print(listy)
 
-""" APPROVED BY AMIR """
+
 #EX4
 
 def unique_val(dictionary):
     dic_list = []
     for key in dictionary:
         dic_list.append(dictionary[key])
-    return list(set(dic_list))
+    return set(dic_list)
 
 dictio4 = {"brand": "Ford", "model": "Mustang", "year": 1964, "year": 1965, "yea2": 1964, "year": 1964}
 print(unique_val(dictio4))
 
-""" Approved by amir """
+
 #EX5
 def lef_rot(list4 ,rotate_n):
     return (list4[rotate_n:] + list4[:rotate_n])
 
 print(lef_rot([1,2,3,4,5,6], 2))
 
-""" approved by amir """
 #EX6
 def print_sec(list5):
     while len(list5):
@@ -60,28 +57,16 @@ def print_sec(list5):
         del list5[::2]
 print(print_sec([1,2,3,4,5,6,7,8,9]))
  
-""" Approved by Amir """
 
 #EX7
 
-def dict_to_tup(dict7):
-    list_frmdic = []
-    for key, value in dict7.items():
-        list_frmdic.append((key, value))
-    return list_frmdic
+digits = [int(i) for i in str(credit_num)]
+    return not (sum(digits[1::2]) + sum(map(GetEverySecondDig ,digits[-2::-2])) ) % 10
+
     
 dict_even =  { 'apples': 10, 'oranges': 12, 'strawberries': 31 }
 print( dict_to_tup(dict_even) )
 
-""" Approved by Amir """
 
-#EX8
-
-my_dict = {'a':10, 'b':5 , 'c': 33, 'd': 51, 'e': 70}
-
-key_max = max(my_dict.keys(), key=(lambda k: my_dict[k]))
-key_min = min(my_dict.keys(), key=(lambda k: my_dict[k]))
-
-print('Maximum Value: ',my_dict[key_max])
-print('Minimum Value: ',my_dict[key_min])
+ #EX7
 
---
+++ python/functions.py
@@ -0,0 +1,65 @@
+#ex1
+
+def remove_from_list(list_ex1, key):
+    print(list(filter(lambda x: x != key, list_ex1)))
+
+ex1_list = ["the", "fox", "jumped", "over", "the", "fence"]
+remove_from_list(ex1_list, "fox")
+
+# ------------------------------------------------------------------------------------
+#ex2
+def sort_str(num_str_li):
+    num_str_li.sort(key=lambda x:x)
+    print(num_str_li)
+
+num_str_li = ["6","3","1","7","2","-2",]
+sort_str(num_str_li)
+
+# -------------------------------------------------------------------------------------
+#ex3
+
+def list_sum(ints_li):
+    total_sum = sum(map(lambda x: int(x), ints_li))
+    print(total_sum)
+
+list_sum([4,-2,-2,1,2,3])
+
+# -------------------------------------------------------------------------------------
+#ex4
+def list_sum(int_li):
+    new_int_list = [x * x for x in int_li if (x % 2 == 0) in int_li]
+    print(new_int_list)
+
+list_sum([1,2,3,4,5])
+
+# -------------------------------------------------------------------------------------
+#ex5
+
+def sale_func(prod_table):
+    prod_table.update( (key, val * 0.9)for key, val in prod_table.items() )
+    print(prod_table)
+
+sale_func({"bread": 6, "milk":  7, "bamba": 8, "bisli": 9})
+
+
+# -------------------------------------------------------------------------------------
+#ex6
+def gematria_cal(user_input):
+    gematria_dic = {"א":1, "ב":2, "ג":3, "ד":4, "ה":5, "ו":6, "ז":7, "ח":8, "ט":9, "י":10,"ך":20,"כ":20, "ל":30,"ם":40, "מ":40, 
+    "נ":50,"ן":50, "ס":60, "ע":70, "ף":80, "פ":80, "צ":90, "ק":100, "ר":200, "ש":300, "ת":400, " ":0}
+    gem_sum = 0
+    for x in user_input: 
+        gem_sum += gematria_dic[x]
+    print(gem_sum)
+    
+gematria_cal("אינפיניטי לאבס")
+
+# -------------------------------------------------------------------------------------
+#ex7
+
+def is_card_valid(card_num):
+    digits = [int(i) for i in str(card_num)]
+    print(0 == (sum(digits[1::2]) + sum(map(card_num ,digits[-2::-2])) ) % 10 )
+ 
+print(  is_card_valid("1230298592834357")   )
+  
