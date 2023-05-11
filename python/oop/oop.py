974031fb0f806a6556507964231f3a78a51e483c
elad <eladgoodname@gmail.com>
2023-05-11 12:20:51 +0300

    Python

--- python/oop/oop.py
+++ python/oop/oop.py
@@ -72,3 +72,72 @@ assert(0 == new_list.len())
 assert(True == new_list.Is_Empty())
 
 
+# ex3 --------------------------------------------------
+
+import datetime
+import time
+
+
+ 
+class Machine:
+    def __init__(self):
+        self.time_started = 0
+        self.cost_per_minute = 0
+        self.is_active = 0
+        self.total_time = 0
+
+    def start_machine(self, machine_type):       
+        self.is_active = 1
+        if machine_type == 1:
+            self.cost_per_minute = 2
+        elif machine_type == 2:
+            self.cost_per_minute = 5
+        self.time_started = datetime.datetime.now()
+
+    def check_time_active(self):
+        self.total_time += self.is_active * ( datetime.datetime.now() - self.time_started ).total_seconds() / 60
+        return self.total_time 
+    
+    def check_total_cost(self):
+        self.check_time_active()
+        return self.cost_per_minute * self.total_time
+    
+    def stop_machine(self):
+        self.check_time_active()
+        self.is_active = 0
+
+ 
+
+a = Machine()
+b = Machine()
+c = Machine()
+d = Machine()
+e = Machine()
+
+a.start_machine(1)
+b.start_machine(1)
+c.start_machine(1)
+d.start_machine(2)
+
+time.sleep(60)
+
+e.start_machine(2)
+
+time.sleep(60)
+
+b.stop_machine()
+
+time.sleep(60)
+
+machine_rig = [a,b,c,d,e]
+
+def machines_total_cost(machine_rig):
+    total_price = 0
+    for machine in machine_rig:
+      total_price += machine.check_total_cost()
+    return total_price
+
+
+print(machines_total_cost(machine_rig))
+
+ 
