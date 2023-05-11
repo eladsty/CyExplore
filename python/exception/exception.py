02a86be5fa67bb97786184ab0073c7df84812823
elad <eladgoodname@gmail.com>
2023-05-11 17:54:44 +0300

    ad

--- python/exception/exception.py
+++ python/exception/exception.py
@@ -1,42 +1,48 @@
 
+
 def check_pwd(password):
-     cond1=0
-     cond2=0
-     cond3=0
-     cond4=0
-     exceptions= ""
-     
+     error = False
      try:
         if len(password) < 8:
-            raise Exception("password length should be 8 or longer") 
+            raise Exception("password must be at least 8 characters") 
      except Exception as len_err:
-         error = True
-         return False
-    
-     for l in password:
-         if l in '[A-Z]':
-             cond1 = 1
-         if l in '[a-z]':
-             cond2 = 1
-         if ( ord(l) in range(ord('0'), ord('9')) ):
-             cond3 = 1
-         if l == '@' or l == '#' or l == '&' or l == '%':
-             cond4 = 1
-    
-         if cond1 + cond2 + cond3 + cond4 == 4: 
-            return True
-     else:
-         if cond1 == 0:
-             exceptions += "password must contain capital letters"
-         if cond2 == 0:
-             exceptions += "password must contain lower case letters"
-         if cond3 == 0:
-             exceptions += "password must contain numbers "
-         if cond4 == 0:
-             exceptions += "password must contain a least one @ # & or %"
-        
+          print("Type of exception:",e)
+          error = True
+     try:
+        if not any(l.isupper() for l in password):
+            raise Exception(" Password must contain at least one capital letter")
+     except Exception as l:
+          print("Type of exception:",e)
+          error = True
+     try:
+        if not any(l.isdigit() for l in password):
+            raise Exception(" Password must contain at least one digit")
+     except Exception as l:
+             print("Type of exception:",e)             
+             error = True
+     try:
+        if not any(l.islower() for l in password):
+            raise Exception(" Password must contain at least one lower case letter")
+     except Exception as l:
+             print("Type of exception:",e)             
+             error = True 
+     try:
+        special_chars = "@#&%"
+        if not any(l in special_chars for l in password):
+            raise Exception(" Password must contain at least one of these symbols: @#&%")
+     except Exception as l:
+             print("Type of exception:",e)             
+             error = True
+
+     if not error:
+          print(" valid password")
+
+
+check_pwd("234234234234")
+
+def TestDriver():
+    passwords = ["283fF9a8j%", "dq#2Sf", "12312312"]
+
+    for pw in passwords:
+        check_pwd(pw)
  
-
-print(check_pwd("283fF9a8j%"))
-print(check_pwd("dq#Sf"))
-
