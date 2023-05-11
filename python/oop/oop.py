ea2bec875f1bd58274a5f75c63b1e70776c01ebd
elad <eladgoodname@gmail.com>
2023-05-11 12:37:21 +0300

    python

--- python/oop/oop.py
+++ python/oop/oop.py
@@ -106,37 +106,37 @@ class Machine:
         self.is_active = 0
 
  
+def test_driver():
+    a = Machine()
+    b = Machine()
+    c = Machine()
+    d = Machine()
+    e = Machine()
 
-a = Machine()
-b = Machine()
-c = Machine()
-d = Machine()
-e = Machine()
+    a.start_machine(1)
+    b.start_machine(1)
+    c.start_machine(1)
+    d.start_machine(2)
 
-a.start_machine(1)
-b.start_machine(1)
-c.start_machine(1)
-d.start_machine(2)
+    time.sleep(60)
 
-time.sleep(60)
+    e.start_machine(2)
 
-e.start_machine(2)
+    time.sleep(60)
 
-time.sleep(60)
+    b.stop_machine()
 
-b.stop_machine()
+    time.sleep(60)
 
-time.sleep(60)
+    machine_rig = [a,b,c,d,e]
 
-machine_rig = [a,b,c,d,e]
-
-def machines_total_cost(machine_rig):
-    total_price = 0
-    for machine in machine_rig:
-      total_price += machine.check_total_cost()
-    return total_price
+    def machines_total_cost(machine_rig):
+        total_price = 0
+        for machine in machine_rig:
+            total_price += machine.check_total_cost()
+        return total_price
 
 
-print(machines_total_cost(machine_rig))
+    print(machines_total_cost(machine_rig))
 
- 
+test_driver()
