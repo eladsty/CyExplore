4633c7c08203616da90adbd917dd345e3bb48be4
elad <eladgoodname@gmail.com>
2023-05-21 20:04:27 +0300

    a

--- cyber/src/transport_layer/ip_sweeper.py
+++ cyber/src/transport_layer/ip_sweeper.py
@@ -14,17 +14,9 @@ def ip_scanner():
 
     print("Scanning Target")
     ip_range = [str(ip) for ip in ipaddress.IPv4Network(target_ip)]
-    net1 = raw_input('Enter the IP address')
-    net2 = net1.split('.')
-    a = '.'
-    net3 = net2[0] + a + net2[1] + a + net2[2] + a
-    stn1 = int(raw_input("enter the frist Number "))
-    edn1 = int(raw_input("enter the last Number "))
-    edn1 = edn1 + 1
-    td1 = datetime.now()
-    
     for ip in ip_range:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
         result = s.connect_ex((ip, int(target_port)))
         if result == 0:
             print(f"Port is open on {ip}")
