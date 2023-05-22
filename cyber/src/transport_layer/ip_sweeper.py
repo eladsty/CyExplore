ff06cca3827bbc2669b7534b150b4fae99f15e35
elad <eladgoodname@gmail.com>
2023-05-22 10:34:06 +0300

    a

--- cyber/src/transport_layer/ip_sweeper.py
+++ cyber/src/transport_layer/ip_sweeper.py
@@ -3,8 +3,7 @@ import socket
 from datetime import datetime
 import ipaddress
 
-def ip_scanner():
-      
+def ip_scanner(): 
     if len(sys.argv) == 3:	
         target_port = sys.argv[1]#the port to scan
         target_ip = sys.argv[2] #ip to scan *should be used with subnet 8 or 08
@@ -15,11 +14,15 @@ def ip_scanner():
     print("Scanning Target")
     ip_range = [str(ip) for ip in ipaddress.IPv4Network(target_ip)]
     for ip in ip_range:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(2)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
+        s.settimeout(0.01)
+        
         result = s.connect_ex((ip, int(target_port)))
         if result == 0:
             print(f"Port is open on {ip}")
         s.close()
         
 ip_scanner()
+
+
+# python3 ip_sweeper.py 22 10.1.0.0/24
