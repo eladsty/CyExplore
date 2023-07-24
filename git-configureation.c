79b9e037714bc04f7fe9e412cfb8450a14ce88f2
elad <eladgoodname@gmail.com>
2023-07-24 11:09:56 +0300

    vpn

---
+++ git-configureation.c
@@ -0,0 +1,10 @@
+[user]
+	email = eladgoodname@gmail.com
+	name = elad
+[credential "https://git.infinitylabs.co.il/ilrd/ramat-gan/cr4/elad.shem-tov"]
+    username = elad.shem-tov
+[credential]
+    helper = store
+
+
+https://elad.shem-tov:201587151@git.infinitylabs.co.il
