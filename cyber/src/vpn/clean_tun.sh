ddaee14191d156f5061cd41f0c51c56bd1525a5f
elad <eladgoodname@gmail.com>
2023-07-24 12:41:46 +0300

    vpn

---
+++ cyber/src/vpn/clean_tun.sh
@@ -0,0 +1,6 @@
+sudo ifconfig tun0 down
+sudo ip link set tun0 down
+sudo ip link delete tun0
+sudo systemctl restart systemd-networkd
+sudo ip addr flush dev enp0s3
+sudo systemctl status systemd-networkd
